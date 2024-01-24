8a7f867146ee0ee8fa714746d31b623eba776a82
Haris317 <mh3401115@gmail.com>
2024-01-24 15:16:38 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,32 @@
+let a = prompt("Please input your distance")
+if(a <= 5){
+    alert("charged 10 rupees from your card")
+}
+else if(a <= 10 ){
+    alert("charged 15 rupees from your card")
+}
+else if(a <= 15 ){
+    alert("charged 20 rupees from your card")
+}
+else if(a <= 20 ){
+    alert("charged 25 rupees from your card")
+}
+else if(a <= 25 ){
+    alert("charged 30 rupees from your card")
+}
+else if(a <= 30 ){
+    alert("charged 35 rupees from your card")
+}
+else if(a <= 35 ){
+    alert("charged 40 rupees from your card")
+}
+else if(a <= 40 ){
+    alert("charged 45 rupees from your card")
+}
+else if(a = 40){
+    alert("charged 50 rupees from your card")
+}
+
+
+
+ 
